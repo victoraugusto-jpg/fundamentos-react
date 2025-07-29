69aacb5c475514efa9a491a8016965046d5b6021
victoraugusto-jpg <victor.2022024077@alunocesul.com>
2025-07-29 20:17:23 -0300

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,10 @@
+import { Provider } from "../components/ui/provider";
+import { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+    return (
+        <Provider>
+            <Component {...pageProps} />
+        </Provider>
+    );
+}
