247c7a4f83a9f4bc7127041d7c7dac54b1685cc9
victoraugusto-jpg <victor.2022024077@alunocesul.com>
2025-07-29 20:24:12 -0300

    add editor config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,9 @@ import { Provider } from "../components/ui/provider";
 import { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-    return (
-        <Provider>
-            <Component {...pageProps} />
-        </Provider>
-    );
+  return (
+    <Provider>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,13 @@
 import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
-    return (
-        <Html suppressHydrationWarning>
-            <Head />
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    );
+  return (
+    <Html suppressHydrationWarning>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
