b046a01562e8674fb53b3f81b024983b7c47ccca
victoraugusto-jpg <victor.2022024077@alunocesul.com>
2025-07-29 21:53:57 -0300

    config do theme

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -1,15 +1,20 @@
-"use client"
+"use client";
 
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
 import {
-  ColorModeProvider,
-  type ColorModeProviderProps,
-} from "./color-mode"
+  ChakraProvider,
+  createSystem,
+  defaultConfig,
+  defaultSystem,
+} from "@chakra-ui/react";
+import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode";
+import { themeConfig } from "@/src/styles/theme";
+
+const theme = createSystem(defaultConfig, themeConfig);
 
 export function Provider(props: ColorModeProviderProps) {
   return (
-    <ChakraProvider value={defaultSystem}>
+    <ChakraProvider value={theme}>
       <ColorModeProvider {...props} />
     </ChakraProvider>
-  )
+  );
 }
---
+++ src/styles/theme.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "@chakra-ui/react";
+
+export const themeConfig = defineConfig({
+  globalCss: {
+    "html, body": {
+      backgroundColor: "gray.100",
+    },
+  },
+  theme: {
+    tokens: {
+      colors: {},
+    },
+  },
+});
