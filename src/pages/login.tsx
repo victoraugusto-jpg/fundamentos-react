2efadbf491cfe33872c2907d1181368fceed95e6
victoraugusto-jpg <victor.2022024077@alunocesul.com>
2025-08-05 19:02:13 -0300

    ajustes tela de login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,3 +1,17 @@
+import { Flex, Heading, Image, Stack, VStack } from "@chakra-ui/react";
+
+import loginImage from "../../public/assets/login-image.gif";
+import { BsStackOverflow } from "react-icons/bs";
+
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <Flex w="100vw" h="100vh" bg="white">
+      <Flex w="50%" bg="#2c73eb" align="center" justify="center">
+        <Image w={500} h={500} src={loginImage.src} />
+      </Flex>
+      <VStack w="50%" bg="green.400" align="start">
+        <Heading>Account Login</Heading>
+      </VStack>
+    </Flex>
+  );
 }
