c98b1fccbad8a6ec29450355fac24d63fb11083c
victoraugusto-jpg <victor.2022024077@alunocesul.com>
2025-08-05 20:44:50 -0300

    ajustes finos tela de login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,19 @@
-import { Flex, Heading, Image, Stack, VStack } from "@chakra-ui/react";
+import {
+  Button,
+  Link as ChakraLink,
+  Flex,
+  Heading,
+  HStack,
+  Image,
+  Input,
+  Stack,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import NextLink from "next/link";
 
 import loginImage from "../../public/assets/login-image.gif";
-import { BsStackOverflow } from "react-icons/bs";
+import { Checkbox } from "../components/ui/checkbox";
 
 export default function Login() {
   return (
@@ -9,8 +21,46 @@ export default function Login() {
       <Flex w="50%" bg="#2c73eb" align="center" justify="center">
         <Image w={500} h={500} src={loginImage.src} />
       </Flex>
-      <VStack w="50%" bg="green.400" align="start">
-        <Heading>Account Login</Heading>
+      <VStack w="50%" bg="white" justify="center">
+        <Stack>
+          <Heading as="h1" color="black" fontSize="3xl" fontWeight="bold">
+            Entrar
+          </Heading>
+          <Text color="gray.400" fontWeight="normal" fontSize="lg">
+            Se você já é membro, você pode fazer login com seu endereço de
+            e-mail e senha.
+          </Text>
+          <VStack align="flex-start" gap={6}>
+            <Input />
+            <Input />
+            <Checkbox
+              colorPalette="blue"
+              color="gray.500"
+              fontSize="md"
+              fontWeight="medium"
+            >
+              Lembre-me
+            </Checkbox>
+            <Button
+              w="full"
+              h={16}
+              borderRadius="md"
+              fontSize="md"
+              fontWeight="medium"
+              colorPalette="blue"
+            >
+              Entrar
+            </Button>
+          </VStack>
+          <HStack justify="center" gap={1} mt={10}>
+            <Text color="gray.500" fontSize="md" fontWeight="medium">
+              Não possui uma conta?
+            </Text>
+            <ChakraLink color="blue.500" asChild>
+              <NextLink href="/sign-up">Clique aqui</NextLink>
+            </ChakraLink>
+          </HStack>
+        </Stack>
       </VStack>
     </Flex>
   );
---
+++ src/pages/sign-up.tsx
@@ -0,0 +1,9 @@
+import { Center } from "@chakra-ui/react";
+
+export default function SignUp() {
+  return (
+    <Center h="100vh" bgColor="red">
+      Criar conta
+    </Center>
+  );
+}
