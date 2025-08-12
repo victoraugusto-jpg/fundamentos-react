cf3e42d8cd838057d82a147fa0d02d66a83d3397
victoraugusto-jpg <victor.2022024077@alunocesul.com>
2025-08-12 20:45:07 -0300

    implementação dos cookies usando uma biblioteca

--- src/contexts/SessionContext.tsx
+++ src/contexts/SessionContext.tsx
@@ -1,4 +1,13 @@
+import { createLazyResult } from "next/dist/server/lib/lazy-result";
+import { destroyCookie, setCookie } from "nookies";
 import { createContext, ReactNode, useContext, useState } from "react";
+
+const CESUL_USER = "cesul.user";
+
+type Credentials = {
+  username: string;
+  password: string;
+};
 
 type User = {
   id: string;
@@ -15,6 +24,14 @@ type SessionContextData = {
 
 const SessionContext = createContext({} as SessionContextData);
 
+export async function signIn({ username, password }: Credentials) {}
+
+export async function signOut() {
+  destroyCookie(null, CESUL_USER, {
+    path: "/",
+  });
+}
+
 interface SessionProviderProps {
   children: ReactNode;
 }
@@ -23,6 +40,17 @@ export function SessionProvider({ children }: SessionProviderProps) {
   const [user, setUser] = useState<User>({} as User);
 
   async function updateUser(user: User) {
+    createCookieUser(user);
+  }
+
+  async function createCookieUser(user: User) {
+    setCookie(null, CESUL_USER, JSON.stringify(user), {
+      path: "/",
+      httpOnly: false,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "Strict",
+    });
+
     setUser(user);
   }
 
